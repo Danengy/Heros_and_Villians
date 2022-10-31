6f839d6ec5f3eb41f9a5b960ee5a526bbe942d03
Daneng <yangdaneng77@gmail.com>
2022-10-31 14:34:15 -0500

    Tried changing arrays in fields in super app's serializer to be able to create new super in Postman

--- supers/views.py
+++ supers/views.py
@@ -10,21 +10,7 @@ from .serializers import SupersSerializer
 def supers_list(request):
 
     if request.method == 'GET':
-        heroes = Supers.objects.filter(super_type__type='Hero')
-        villians = Supers.objects.filter(super_type__type='Villian')
-        heroes_serializer = SupersSerializer(heroes, many=True)
-        villian_serializer = SupersSerializer(villians, many=True)
-        super_type_param = request.query_params.get('super_type')
-        print(super_type_param)
         supers = Supers.objects.all()
-        if super_type_param:
-            supers =supers.filter(super_type__type=super_type_param)
-        elif not super_type_param:
-            custom_reponse = {
-                'Hero' : heroes_serializer.data,
-                'Villian': villian_serializer.data
-            }
-            return Response(custom_reponse)
         serializer = SupersSerializer(supers, many = True)
         return Response(serializer.data)
     
@@ -52,3 +38,18 @@ def supers_detail(request, pk):
 
 
 
+@api_view(['GET'])
+def heroes_villians(request):
+    heroes = Supers.objects.filter(super_type__type='Hero')
+    villians = Supers.objects.filter(super_type__type='Villian')
+    heroes_serializer = SupersSerializer(heroes, many=True)
+    villian_serializer = SupersSerializer(villians, many=True)
+    super_type_param = request.query_params.get('super_type')
+    if super_type_param:
+            supers =supers.filter(super_type__type=super_type_param)
+    elif not super_type_param:
+        custom_reponse = {
+            'Hero' : heroes_serializer.data,
+            'Villian': villian_serializer.data
+        }
+        return Response(custom_reponse)
